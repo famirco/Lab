48b5cfe6675efe9be2b818f5c6b3f8ad861f1408
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-08-09 16:17:40 +0430

    step-0.3

--- TerraformInstallation.sh
+++ TerraformInstallation.sh
@@ -1,73 +1,132 @@
 #!/usr/bin/bash
 
-#change to main directory
-sudo rm -rf /myproject
-sudo mkdir /myproject
-cd /myproject
+sudo rm -rf Myproject
+sudo mkdir Myproject
+
+
+if ls Myproject 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'Myproject directory has been created successfully.'
+  else
+  echo '.**######################################################################################**.'
+  echo 'WE can not create directory.'
+  exit 1
+fi
+
+cd Myproject
 
 #install the HashiCorp GPG key.
-sudo wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+if cat /usr/share/keyrings/hashicorp-archive-keyring.gpg 1>/dev/null 2>&1; then
+  echo '.**######################################################################################**.'
+  echo 'HashiCorp GPG key is already installed.'
+  else
+  sudo wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo 'Install HashiCorp GPG key completed successfully.'
+fi
+
 
 #add the official HashiCorp repository to your system.
-sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+if cat /etc/apt/sources.list.d/hashicorp.list 1>/dev/null 2>&1; then
+  echo '.**######################################################################################**.'
+  echo 'The official HashiCorp repository already have to your system.'
+  else
+  sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo 'The official HashiCorp repository has been added to your system.'
+fi
 
 #update repositories
-sudo apt update -y
+sudo apt update -y 1>/dev/null
+echo '.**######################################################################################**.'
+echo 'Apt update completed successfully.'
 
-#install HashiCorp's Debian package repository.
-sudo apt-get install -y gnupg software-properties-common
 
 #verify the key's fingerprint.
+echo '.**######################################################################################**.'
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
-<<comment
-The gpg command will report the key fingerprint:
+
+echo 'The gpg command will report the key fingerprint:
 /usr/share/keyrings/hashicorp-archive-keyring.gpg
 -------------------------------------------------
 pub   rsa4096 2020-05-07 [SC]
       E8A0 32E0 94D8 EB4E A189  D270 DA41 8C88 A321 9F7B
 uid           [ unknown] HashiCorp Security (HashiCorp Package Signing) <security+packaging@hashicorp.com>
-sub   rsa4096 2020-05-07 [E]
-comment
+sub   rsa4096 2020-05-07 [E]'
 
 
 #check dependancies and install them
-if ! unzip -h 2&> /dev/null; then
-  sudo apt install -y unzip
+if unzip -h 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'Unzip is already installed.'
+  else
+  sudo apt install -y unzip 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( Unzip ) Installation completed successfully.'
 fi
 
-if ! curl -h 2&> /dev/null; then
-  sudo apt install  -y curl
+if curl -h 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'curl is already installed.'
+  else
+  sudo apt install  -y curl 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( curl ) Installation completed successfully.'
 fi
 
-if ! wget -h 2&> /dev/null; then
-  sudo apt install  -y wget
+if wget -h 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'wget is already installed.'
+  else
+  sudo apt install  -y wget 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( wget ) Installation completed successfully.'
 fi
 
-if ! jq --version 2&> /dev/null; then
-  sudo apt install -y jq
+if jq --version 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'jq is already installed.'
+  else
+  sudo apt install -y jq 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( jq ) Installation completed successfully.'
 fi
 
 #install virtualbox for vagrant
-if ! vboxmanage -h 2&> /dev/null; then
- sudo apt install -y virtualbox
+if vboxmanage -h 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'Virtualbox is already installed.'
+  else
+  sudo apt install -y virtualbox 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( Virtualbox ) Installation completed successfully.'
 fi
 
 #install vagrant
-if ! vagrant -h 2&> /dev/null; then
-  sudo apt install -y vagrant
+if vagrant -h 1>/dev/null; then
+  echo '.**######################################################################################**.'
+  echo 'Vagrant is already installed.'
+  else
+  sudo apt install -y vagrant 1>/dev/null
+  echo '.**######################################################################################**.'
+  echo '( Vagrant ) Installation completed successfully.'
 fi
 
 #install terraform
-sudo apt-get install -y terraform
-
-#install the autocomplete package.
-sudo terraform -install-autocomplete
+sudo apt-get install -y terraform 1>/dev/null
+echo '.**######################################################################################**.'
+echo '( Vagrant ) Installation completed successfully.'
 
 #install ansible
-sudo apt install ansible
+sudo apt install ansible 1>/dev/null
+echo '.**######################################################################################**.'
+echo '( Vagrant ) Installation completed successfully.'
 
 #
 vagrant box add ubuntu/focal64
+echo '.**######################################################################################**.'
+echo '( ubuntu/focal64 ) Added to box.'
+
 
 #hard add config to terraform file
 echo 'terraform {
@@ -91,38 +150,39 @@ resource "vagrant_vm" "my_vagrant_vm" {
 
 
 echo 'Vagrant.configure("2") do |config|
-    config.vm.box = "ubuntu/focal64"
-    config.vm.network "forwarded_port", guest: 22, host: 221
-  
-    config.vm.provider "VirtualBoxe" do |vb|
-    end
-  
-    config.vm.define "master" do |master|
-      master.vm.box = "ubuntu/focal64"
-      master.vm.hostname = "master"
-      master.vm.provider :VirtualBox do |v|
-        v.customize ['modifyvm', :id, '--memory', '2048']
-        v.customize ['modifyvm', :id, '--cpus', '2']
-      end
-    end
-    config.vm.define "slave1" do |slave1|
-      slave1.vm.box = "ubuntu/focal64"
-      slave1.vm.hostname = "slave1"
-      slave1.vm.provider :VirtualBox do |v|
-        v.customize ['modifyvm', :id, '--memory', '2048']
-        v.customize ['modifyvm', :id, '--cpus', '2']
-      end
-    end
-    config.vm.define "slave2" do |slave2|
-      slave2.vm.box = "ubuntu/focal64"
-      slave2.vm.hostname = "slave2"
-      slave2.vm.provider :VirtualBox do |v|
-        v.customize ['modifyvm', :id, '--memory', '2048']
-        v.customize ['modifyvm', :id, '--cpus', '2']
-      end
-    end
-  end' > Vagrantfile
+  config.vm.box = "ubuntu/focal64"
+#  config.vm.network "forwarded_port", guest: 22, host: 221
 
+  config.vm.provider "VirtualBoxe" do |vb|
+  end
+
+  config.vm.define "ansible" do |ansible|
+    ansible.vm.box = "ubuntu/focal64"
+    ansible.vm.hostname = "ansible"
+    ansible.vm.provider :VirtualBox do |v|
+      v.customize ['modifyvm', :id, '--memory', '2048']
+      v.customize ['modifyvm', :id, '--cpus', '2']
+    end
+  end
+  config.vm.define "ansible" do |ansible|
+    ansible.vm.box = "ubuntu/focal64"
+    ansible.vm.hostname = "ansible"
+    ansible.vm.provider :VirtualBox do |v|
+      v.customize ['modifyvm', :id, '--memory', '2048']
+      v.customize ['modifyvm', :id, '--cpus', '2']
+    end
+  end
+  config.vm.define "ansible" do |ansible|
+    ansible.vm.box = "ubuntu/focal64"
+    ansible.vm.hostname = "ansible"
+    ansible.vm.provider :VirtualBox do |v|
+      v.customize ['modifyvm', :id, '--memory', '2048']
+      v.customize ['modifyvm', :id, '--cpus', '2']
+    end
+  end
+end' > Vagrantfile
+
+vagrant init
 terraform init
 
 
