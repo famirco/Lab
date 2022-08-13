49981d6e8ce4302c1c2e82ed97a94e9b253a446d
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-08-13 15:06:53 +0430

    Creat playbook.

--- Install.sh
+++ Install.sh
@@ -8,7 +8,7 @@ echo '############### Apt update completed successfully.###############'
 sudo apt install -y python3 python3-pip ansible yamllint ansible-lint
 echo '############### Ansible installation completed successfully.###############'
 
-echo 'controlmachine ansible-host=127.0.0.1' >> /etc/ansible/hosts
 
 
-ansible-playbook ansible-playbooks/ConfigureLocal.yml
+
+ansible-playbook -i ansible-playbooks/hosts ansible-playbooks/ConfigureLocal.yml
