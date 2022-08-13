98409d38686ec3d6c431fbec51f29351dda12731
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-08-13 11:26:15 +0430

    check ansible hosts.

--- Install.sh
+++ Install.sh
@@ -1,1 +1,22 @@
-test
+#!/usr/bin/bash
+
+#Update repositories
+sudo apt update -y 1>/dev/null
+echo '############### Apt update completed successfully.###############'
+
+#Install Ansible
+sudo apt install -y ansible
+echo '############### Ansible installation completed successfully.###############'
+
+ansible_host=$(cat /etc/ansible/hosts | awk '{print $2}')
+if [ ! ansible_hos == 'ansible_host=127.0.0.1' ];then
+    echo '[servers]
+server1 ansible_host=127.0.0.1
+
+
+[all:vars]
+ansible_python_interpreter=/usr/bin/python3' >> cat /etc/ansible/hosts
+fi
+
+
+
