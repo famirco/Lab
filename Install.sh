fc88795307d058a03dd8e6f874fbcd0aac2ad6ac
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-08-13 14:55:04 +0430

    Creat playbook.

--- Install.sh
+++ Install.sh
@@ -5,18 +5,10 @@ sudo apt update -y 1>/dev/null
 echo '############### Apt update completed successfully.###############'
 
 #Install Ansible
-sudo apt install -y ansible
+sudo apt install -y python3 python3-pip ansible yamllint ansible-lint
 echo '############### Ansible installation completed successfully.###############'
 
-ansible_host=$(cat /etc/ansible/hosts | awk '{print $2}')
-if [ ! ansible_host == 'ansible_host=127.0.0.1' ];then
-    echo '[servers]
-server1 ansible_host=127.0.0.1
+echo 'controlmachine ansible-host=127.0.0.1' >> /etc/ansible/hosts
 
 
-[all:vars]
-ansible_python_interpreter=/usr/bin/python3' >> cat /etc/ansible/hosts
-fi
-
-
-
+ansible-playbook ansible-playbooks/ConfigureLocal.yml
