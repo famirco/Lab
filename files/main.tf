d7925d68f785e05dcdd1001bf60f791f57945016
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-09-03 09:32:45 +0430

    change to busybox

--- files/main.tf
+++ files/main.tf
@@ -12,12 +12,12 @@ provider "docker" {
 }
 
 # Pulls the image
-resource "docker_image" "ubuntu" {
+resource "docker_image" "busybox" {
   name = "ubuntu:latest"
 }
 
 # Create a container
 resource "docker_container" "foo" {
-  image = docker_image.ubuntu.latest
+  image = docker_image.busybox.latest
   name  = "foo"
 }
