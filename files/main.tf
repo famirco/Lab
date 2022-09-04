f54d760aa09a54ce4ee5547eef02cb8fdc351ced
Amir Dezyanian <amir@Amirs-MacBook-Pro.local>
2022-09-04 11:49:49 +0430

    T-shoot terraform

--- files/main.tf
+++ files/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.20.2"
+      version = "2.20.3"
     }
   }
 }
@@ -12,12 +12,12 @@ provider "docker" {
 }
 
 # Pulls the image
-resource "docker_image" "busybox" {
+resource "docker_image" "ubuntu" {
   name = "ubuntu:latest"
 }
 
 # Create a container
 resource "docker_container" "foo" {
-  image = docker_image.busybox.latest
+  image = docker_image.ubuntu.latest
   name  = "foo"
 }
